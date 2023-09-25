355c15fdbbf4bd86a817b119b0ce811a53a74552
shahin hossain <shahinhossain3@hotmail.com>
2023-09-25 19:50:56 +0600

    Extract Cart products from Products data - localstorage

--- src/Loaders/CartProductLoader.js
+++ src/Loaders/CartProductLoader.js
@@ -1,7 +1,24 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 //একটি আলাদা function লিখলাম data fetch করার জন্য। যেখানে দরকার হবে সেখানে এটি import করে ব্যবহার করা যাবে।
 const CartProductsLoader = async () => {
     const loadedProducts = await fetch('products.json')
     const products = await loadedProducts.json();
-    return products
+    //if cart data in database, i have to use async await নইলে আমরা data কে পাবো না।
+    const storedCart = getShoppingCart(); // local Storage থেকে data load করা হয়েছে।
+    const savedCart = [];
+    for (const id in storedCart) {
+        const addedProduct = products.find(pd => pd.id === id);
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    //if you need to send two things যদি ২টা জিনিস পাঠাতে হয় তাহলে করনীয়
+    // return [products, savedCart]
+    //another option 
+    // return { products, savedCart } // such as {products} = {products: products} product এর মধ্যে product এর ভ্যালু গুলো পাঠানো। 
+    return savedCart;
 }
 export default CartProductsLoader;
--- src/component/Orders/Orders.jsx
+++ src/component/Orders/Orders.jsx
@@ -3,15 +3,15 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
-    const products = useLoaderData();
-    console.log(products)
+    const cart = useLoaderData();
+    console.log(cart)
     return (
         <div className='shop-container'>
             <div className='product-container'>
-                <h1>Orders Page</h1>
+                <h1>Orders Page {cart.length}</h1>
             </div>
             <div>
-                <Cart cart={[]}></Cart>  {/* এখানে Cart কে নেয়া হয়েছে, এখানে যদি কোনো ভ্যালু না দেয়া হয় তাহলে not iterable Error দিয়ে যা কোনো array এর লুপ কে বুঝায় কারণ Cart এর মধ্যে array কে লুপ করা হচ্ছে।*/}
+                <Cart cart={cart}></Cart>  {/* এখানে Cart কে নেয়া হয়েছে, এখানে যদি কোনো ভ্যালু না দেয়া হয় তাহলে not iterable Error দিয়ে যা কোনো array এর লুপ কে বুঝায় কারণ Cart এর মধ্যে array কে লুপ করা হচ্ছে।*/}
             </div>
 
         </div>

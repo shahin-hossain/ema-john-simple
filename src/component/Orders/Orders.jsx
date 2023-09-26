33a8582d360ed81b98dd4e9820e15b027d3564f8
shahin hossain <shahinhossain3@hotmail.com>
2023-09-26 18:53:26 +0600

    Remove Item handler and Make Data consistent across UI

--- src/component/Orders/Orders.jsx
+++ src/component/Orders/Orders.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css'
+import { removeFromDb } from '../../utilities/fakedb';
 const Orders = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
+    // কোনো cart item কে cart list থেকে delete করতে চাইলে নতুন করে state set করে data পাঠাতে হবে।
+    const [cart, setCart] = useState(savedCart) //saveCart এখন শুধু initial State এ ব্যবহার করা হবে।
+    // console.log(savedCart)
+    const handleRemoveFromId = (id) => {
+        const remaining = cart.filter(product => product.id !== id); //filter করা হয়েছে, যে id টা click handler এর সাথে মিলবে সেটা বাদে বাকি product গুলো নিবে।
+        setCart(remaining); //বাকি গুলোকে state এ সেট করে দেয়া হয়েছে।
+        removeFromDb(id) // local Storage থেকেও id Remove করা হয়েছে।
+    }
     return (
         <div className='shop-container'>
             <div className='review-container'>
                 {cart.map(product => <ReviewItem
                     key={product.id}
                     product={product}
+                    handleRemoveFromId={handleRemoveFromId} // function কে props করে পাঠানো হয়েছে।
                 ></ReviewItem>)}
             </div>
             <div>
--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ReviewItem.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
-const ReviewItem = ({ product }) => {
+const ReviewItem = ({ product, handleRemoveFromId }) => {
     const { id, img, name, price, quantity } = product
     return (
         <div className='review-item'>
@@ -12,7 +12,7 @@ const ReviewItem = ({ product }) => {
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Quantity: <span className='orange-text'>{quantity}</span></p>
             </div>
-            <button className='btn-delete'>
+            <button onClick={() => handleRemoveFromId(id)} className='btn-delete' > {/*button এ ক্লিক করলে, item Remove করতে চাইলে এখানে শুধু call করে id টাকে পাঠাতে হবে। যেখান থেকে propos আসছে*/}
                 <FontAwesomeIcon icon={faTrashCan} className='delete-icon' />
             </button>
         </div>

6cb43b6279ebac26b68fda969262cb75e66b895d
shahin hossain <shahinhossain3@hotmail.com>
2023-09-26 17:49:43 +0600

    More style of Review item and font awesome delete style

--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 const ReviewItem = ({ product }) => {
     const { id, img, name, price, quantity } = product
     return (
         <div className='review-item'>
             <img src={img} alt="productImg" />
-
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'>
+                <FontAwesomeIcon icon={faTrashCan} className='delete-icon' />
+            </button>
         </div>
     );
 };

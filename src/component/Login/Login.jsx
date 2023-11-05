caa4e97e0b487cd8fdc4de9731f6a83d3d53e810
shahin hossain <shahinhossain3@hotmail.com>
2023-11-05 21:07:23 +0600

    Create Login component and style Login form

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import './Login.css'
 const Login = () => {
     return (
-        <div>
-            <h2>Login Page</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                {/* form এর মধ্যে যে জিনিসগুলো থাকে তাকে বলা হয় form Control */}
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name='email' id='' placeholder='Enter your email' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name='password' id='' placeholder='Enter your password' required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
         </div>
     );
 };
--- src/component/SignUp/SignUp.jsx
+++ src/component/SignUp/SignUp.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import './SignUp.css'
 const SignUp = () => {
     return (
-        <div>
-            <h2>Please Sign Up</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form>
+                {/* form এর মধ্যে যে জিনিসগুলো থাকে তাকে বলা হয় form Control */}
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name='email' id='' placeholder='Enter your email' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name='password' id='' placeholder='Enter your password' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Confirm Password</label>
+                    <input type="password" name='confirm' id='' placeholder='Enter your password' required />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+            </form>
         </div>
     );
 };
